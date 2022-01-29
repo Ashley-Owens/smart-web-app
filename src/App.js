8383455e4419ebec3eb8631ae7ecad6175b99b47
blakelester <lesterst@oregonstate.edu>
2022-01-29 14:10:12 -0700

    feat: initialize react app project with skeleton home page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './components/NavBar'
+import HomeContent from './components/HomeContent'
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <NavBar />
+            <HomeContent />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/leadStatusTypes.js
@@ -0,0 +1,28 @@
+const leadStatusTypes = [
+    {
+        title: 'New',
+        photo: null,
+    },
+    {
+        title: 'Open',
+        photo: null,
+    },
+    {
+        title: 'Won',
+        photo: null,
+    },
+    {
+        title: 'Lost',
+        photo: null,
+    },
+    {
+        title: 'Sent',
+        photo: null,
+    },
+    {
+        title: 'All Leads',
+        photo: null,
+    },
+];
+
+export default leadStatusTypes;
